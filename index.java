74777e7b5b2de46908de04cf4a8ef7504561f061
praveenk1111 <spraveenk1111@gmail.com>
2022-09-04 13:53:09 +0000

    devops added

--- index.java
+++ index.java
@@ -1,5 +1,5 @@
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!, Hello Sunny D Prince"); 
+        System.out.println("Hello, World!, Hello Sunny D Prince DevOps Engineer"); 
     }
 }
